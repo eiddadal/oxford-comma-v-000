e9f0423deecaa72f00f7f6c97cab92921c46e112
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-26 17:24:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ if array.length == 1
   array.join(" and ")
   elsif array.length >=3
   last_element=array.pop
-  array.join(" ,")+", and "+ last_element
+  array.join(", ")+", and "+ last_element
  
 end 
 end
