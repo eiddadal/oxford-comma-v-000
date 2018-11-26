970e92fc33e6d29246725fea1137e36237684230
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-26 17:22:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,9 @@ if array.length == 1
   array.join 
   elsif array.length ==2 
   array.join(" and ")
+  elsif array.length >=3 
+  array.join(",") + " and " + last
 else 
-  array.join(" and " )
+ 
 end 
 end
