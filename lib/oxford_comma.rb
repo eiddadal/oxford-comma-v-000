4ebb211c8e076f3cec3b55da3c85cc577b19ae82
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-26 17:23:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ if array.length == 1
   array.join 
   elsif array.length ==2 
   array.join(" and ")
-  elsif array.length >=3 
+  elsif array.length >=3
+  last_element=array.pop
   array.join(",") + ", and " + last_element
  
 end 
